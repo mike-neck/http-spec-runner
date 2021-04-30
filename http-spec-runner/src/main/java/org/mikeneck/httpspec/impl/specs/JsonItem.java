2413b0750318184d82f7e65c7639b7fa792feb65
mike_neck <jkrt3333@gmail.com>
2021-04-30 16:29:27 +0900

    refactor to remove unused method in JsonItem

--- http-spec-runner/src/main/java/org/mikeneck/httpspec/impl/specs/JsonItem.java
+++ http-spec-runner/src/main/java/org/mikeneck/httpspec/impl/specs/JsonItem.java
@@ -1,14 +1,6 @@
 package org.mikeneck.httpspec.impl.specs;
 
-import org.jetbrains.annotations.NotNull;
-import org.mikeneck.httpspec.HttpResponseAssertion;
-
 public interface JsonItem {
 
   String describeValue();
-
-  @NotNull
-  default HttpResponseAssertion<?> testJson(@NotNull JsonPathProduct jsonPathProduct) {
-    return jsonPathProduct.assertBy(this);
-  }
 }
