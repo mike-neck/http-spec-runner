d2886997b44f3ce5fa28dd170a5d75a54dde5002
mike_neck <jkrt3333@gmail.com>
2021-05-02 19:08:44 +0900

    refactor UnexpectedResponseException to show more detailed message

--- http-spec-runner/src/main/java/org/mikeneck/httpspec/UnexpectedResponseException.java
+++ http-spec-runner/src/main/java/org/mikeneck/httpspec/UnexpectedResponseException.java
@@ -12,4 +12,23 @@ public class UnexpectedResponseException extends RuntimeException {
     super(message);
     this.results = results;
   }
+
+  @Override
+  public String getMessage() {
+    StringBuilder builder = new StringBuilder(super.getMessage());
+    for (VerificationResult result : results) {
+      StringBuilder sb = new StringBuilder();
+      for (HttpResponseAssertion<?> assertion : result) {
+        if (!assertion.isSuccess()) {
+          sb.append(assertion.description()).append('\n');
+        }
+      }
+      String description = sb.toString();
+      if (!description.isEmpty()) {
+        builder.append('\n').append('\n').append(result.specName()).append('\n').append("====");
+        builder.append('\n').append(description);
+      }
+    }
+    return builder.toString();
+  }
 }
